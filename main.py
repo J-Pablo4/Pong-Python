addcabbf1d816e2e39952f9bfd8750efdb36208e
Juan Pablo <pablo.lopez@iteso.mx>
2022-12-21 17:43:02 -0600

    Created window specifications

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import turtle
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+window = turtle.Screen()
+window.title("Pong by Juan Pablo")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Main game loop
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
